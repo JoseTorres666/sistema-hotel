c15223fb37f2371c7ab43e38efd1f8c30fcb43b6
JoseTorres666 <jose.torres@sisb.edu.ec>
2024-08-05 22:30:04 -0400

    asd

--- app/Controllers/Usuario.php
+++ app/Controllers/Usuario.php
@@ -29,6 +29,7 @@ class Usuario extends BaseController
         echo view('template/header'); 
         echo view('usuario/agregar');
         echo view('template/footer');
+        sad
     }
 
     public function agregarbd()
